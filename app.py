c399cfcfbe7cc809e3f38a8d5f5dcba685b99106
ATLAS-0VPF92L98 <varunjoshi12363@gmail.com>
2023-04-04 15:05:15 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, GHW is here!</p>"
+    return "<p>Hello going to make contribution in open source</p>"
+
+if __name__=="__main__":
+    app.run(debug=True)
